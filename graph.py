6c8c219fe9b1ccbb70a1923ce11d84b8abda37cf
Erfan Alimohammadi <erfan.aa@gmail.com>
2019-07-12 18:03:58 +0430

    Add Graph class

--- graph.py
+++ graph.py
@@ -5,8 +5,8 @@ from node import Node
 graph = nx.Graph()
 node1, node2 = Node(1, 10, 10), Node(2, 20, 20)
 
-graph.add_node(1, pos=(10, 10))
-graph.add_node(2, pos=(20, 20))
+graph.add_node(node1.index, pos=(node1.x, node1.y))
+graph.add_node(node2.index, pos=(node2.x, node2.y))
 
 print(graph.nodes[1]['pos'])
 print(graph.nodes[2]['pos'])
@@ -21,3 +21,28 @@ print(graph.edges())
 pos = nx.get_node_attributes(graph, 'pos')
 nx.draw(graph, pos)
 plt.show()
+
+
+class Graph:
+
+	def __init__(self, nodes=None, edges=None):
+		self.nodes = []
+		self.edges = []
+		if nodes:
+			self.nodes = nodes
+		if edges:
+			self.edges = edges
+
+	def add_edge(self, edge):
+		self.edges.append(edge)
+
+	def add_node(self, node):
+		self.nodes.append(node)
+
+	def get_nx_graph(self):
+		graph = nx.Graph()
+		for node in nodes:
+			graph.add_node(node.index, pos=(node.x, node.y))
+		for edge in edges:
+			graph.add_edge(edge[0], edge[1])
+		return graph
